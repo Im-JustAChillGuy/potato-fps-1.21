6773530c455105560a23e81a0e613a627f6beec6
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-07-13 17:17:21 +0530

    Update PotatoConfig.java

--- src/main/java/com/example/potatofps/PotatoConfig.java
+++ src/main/java/com/example/potatofps/PotatoConfig.java
@@ -1,28 +1,63 @@
-/*
- * Decompiled with CFR 0.152.
- * 
- * Could not load the following classes:
- *  net.fabricmc.api.EnvType
- *  net.fabricmc.api.Environment
- */
 package com.example.potatofps;
 
+import com.example.potatofps.AdjustmentSpeed;
+import com.example.potatofps.PotatoConfig;
+import com.example.potatofps.PotatoConfigManager;
+import me.shedaniel.clothconfig2.api.AbstractConfigListEntry;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.text.Text;
+import net.minecraft.client.gui.screen.Screen;
 
 @Environment(value=EnvType.CLIENT)
-public class PotatoConfig {
-    public static boolean potatoMode = true;
-    public static boolean disableShadows = true;
-    public static boolean disableClouds = true;
-    public static boolean disableVignette = true;
-    public static boolean ultraParticleReduction = false;
-    public static boolean reduceEntityDistance = true;
-    public static boolean disableRainParticles = true;
-    public static int minRender = 4;
-    public static int maxRender = 16;
-    public static int targetFps = 70;
-    public static boolean showHud = true;
-    public static int adjustmentSpeed = 1;
-    public static int entityRenderDistance = 64;
+public class PotatoConfigScreen {
+    public static Screen create(Screen parent) {
+        ConfigBuilder builder = ConfigBuilder.create().setParentScreen(parent).setTitle(Text.literal((String)"Potato FPS Settings")).setSavingRunnable(() -> PotatoConfigManager.save());
+        ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+        ConfigCategory general = builder.getOrCreateCategory(Text.literal((String)"Performance"));
+        general.addEntry((AbstractConfigListEntry)entryBuilder.startBooleanToggle(Text.literal((String)"Enable Potato Mode"), PotatoConfig.potatoMode).setDefaultValue(true).setSaveConsumer(newValue -> {
+            PotatoConfig.potatoMode = newValue;
+        }).build());
+        general.addEntry((AbstractConfigListEntry)entryBuilder.startBooleanToggle(Text.literal((String)"Disable Entity Shadows"), PotatoConfig.disableShadows).setDefaultValue(true).setSaveConsumer(newValue -> {
+            PotatoConfig.disableShadows = newValue;
+        }).build());
+        general.addEntry((AbstractConfigListEntry)entryBuilder.startBooleanToggle(Text.literal((String)"Disable Clouds"), PotatoConfig.disableClouds).setDefaultValue(true).setSaveConsumer(newValue -> {
+            PotatoConfig.disableClouds = newValue;
+        }).build());
+        general.addEntry((AbstractConfigListEntry)entryBuilder.startBooleanToggle(Text.literal((String)"Disable Vignette"), PotatoConfig.disableVignette).setDefaultValue(true).setSaveConsumer(newValue -> {
+            PotatoConfig.disableVignette = newValue;
+        }).build());
+        general.addEntry((AbstractConfigListEntry)entryBuilder.startBooleanToggle(Text.literal((String)"Ultra Particle Reduction"), PotatoConfig.ultraParticleReduction).setDefaultValue(false).setSaveConsumer(newValue -> {
+            PotatoConfig.ultraParticleReduction = newValue;
+        }).build());
+        general.addEntry((AbstractConfigListEntry)entryBuilder.startBooleanToggle(Text.literal((String)"Reduce Entity Render Distance"), PotatoConfig.reduceEntityDistance).setDefaultValue(true).setSaveConsumer(newValue -> {
+            PotatoConfig.reduceEntityDistance = newValue;
+        }).build());
+        general.addEntry((AbstractConfigListEntry)entryBuilder.startBooleanToggle(Text.literal((String)"Disable Rain Splash Particles"), PotatoConfig.disableRainParticles).setDefaultValue(true).setSaveConsumer(newValue -> {
+            PotatoConfig.disableRainParticles = newValue;
+        }).build());
+        general.addEntry((AbstractConfigListEntry)entryBuilder.startIntSlider(Text.literal((String)"Minimum Render Distance"), PotatoConfig.minRender, 2, 32).setDefaultValue(8).setSaveConsumer(newValue -> {
+            PotatoConfig.minRender = newValue;
+        }).build());
+        general.addEntry((AbstractConfigListEntry)entryBuilder.startIntSlider(Text.literal((String)"Maximum Render Distance"), PotatoConfig.maxRender, 4, 32).setDefaultValue(16).setSaveConsumer(newValue -> {
+            PotatoConfig.maxRender = newValue;
+        }).build());
+        general.addEntry((AbstractConfigListEntry)entryBuilder.startIntSlider(Text.literal((String)"Target FPS"), PotatoConfig.targetFps, 30, 240).setDefaultValue(70).setSaveConsumer(newValue -> {
+            PotatoConfig.targetFps = newValue;
+        }).build());
+        general.addEntry((AbstractConfigListEntry)entryBuilder.startBooleanToggle(Text.literal((String)"Show Potato HUD"), PotatoConfig.showHud).setDefaultValue(true).setSaveConsumer(newValue -> {
+            PotatoConfig.showHud = newValue;
+        }).build());
+        general.addEntry((AbstractConfigListEntry)entryBuilder.startEnumSelector(Text.literal((String)"Adjustment Speed"), AdjustmentSpeed.class, (Enum)AdjustmentSpeed.values()[PotatoConfig.adjustmentSpeed]).setDefaultValue((Enum)AdjustmentSpeed.SLOW).setSaveConsumer(newValue -> {
+            PotatoConfig.adjustmentSpeed = newValue.ordinal();
+        }).build());
+        general.addEntry((AbstractConfigListEntry)entryBuilder.startIntSlider(Text.literal((String)"Entity Render Distance"), PotatoConfig.entityRenderDistance, 16, 128).setDefaultValue(64).setSaveConsumer(value -> {
+            PotatoConfig.entityRenderDistance = value;
+            PotatoConfigManager.save();
+        }).build());
+        return builder.build();
+    }
 }
